cc63dfa9e6dfaf6d0b6168598f3ecec24fb5ec75
NebulaServicesA <162523687+NebulaServicesA@users.noreply.github.com>
2024-12-07 15:49:31 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,18 +1,1 @@
 
-function openGame(gameName) {
-    const gameLinks = {
-        "subway-surfers": "https://ly4fsf.csb.app/",
-        "basket-random": "https://images-opensocial.googleusercontent.com/gadgets/ifr?url=https://googleusercontent.b-cdn.net/basket-random/basran.xml&container=ig",
-        "drive-mad": "https://drivemad.me/iframe/index.html"
-        // Add more games here
-    };
-    const gameFrame = document.getElementById("game-frame");
-    gameFrame.src = gameLinks[gameName];
-    document.getElementById("game-modal").classList.remove("hidden");
-}
-
-function closeGame() {
-    const gameFrame = document.getElementById("game-frame");
-    gameFrame.src = "";
-    document.getElementById("game-modal").classList.add("hidden");
-}
